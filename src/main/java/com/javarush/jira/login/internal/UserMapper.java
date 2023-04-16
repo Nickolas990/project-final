f443157251a29e4e180fbb0c6766cdb85d44192f
Nickolas990 <hrom_90@hotmail.com>
2023-04-16 23:32:42 +0300

    Vk and Yandex login options deleted

--- src/main/java/com/javarush/jira/login/internal/UserMapper.java
+++ src/main/java/com/javarush/jira/login/internal/UserMapper.java
@@ -7,9 +7,11 @@ import com.javarush.jira.login.UserTo;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.springframework.stereotype.Component;
 
 import java.util.EnumSet;
 
+@Component
 @Mapper(componentModel = "spring", imports = {EnumSet.class, Role.class})
 public interface UserMapper extends BaseMapper<User, UserTo> {
     @Override
